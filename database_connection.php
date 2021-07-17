6ebf6f7773cb4e5ee46849618b5c7bd62fa3f7ab
Hassam Ul Haq <58927204+hassamulhaq@users.noreply.github.com>
2021-07-17 16:59:45 +0500

    Delete database_connection.php

--- database_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-    $conn = new PDO("mysql: host=localhost; dbname=salon_db", "root", "");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die ($e->getMessage());
-}
